1f17ee045cf368ee0296932f821f64f4ab2304e6
Allan <oallanmendes@gmail.com>
2020-11-17 00:03:34 -0300

    changed delay

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config';
+
 import testQueue from './lib/testQueue';
+
+let i = 0;
 
 setInterval(() => {
   testQueue.add('sendHello', {
     message: `Hello world`,
   });
-  console.log('Job scheduled');
-}, 10);
+  console.log(`Job scheduled -> Agendamento ${i}`);
+  i += 1;
+}, 10000);
